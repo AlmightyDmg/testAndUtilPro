3bd0b1cd3ecd7089d9e362197051edeebfa6e409
zhum <zhumeng@skyvis.com.cn>
2019-07-24 15:22:41 +0800

    Initial commit

---
+++ src/main/java/cn/com/test/Test.java
@@ -0,0 +1,76 @@
+package cn.com.test;
+
+public class Test {
+    String a = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "\n" +
+            "<GtRecord>\n" +
+            "  <case>\n" +
+            "    <saay>3368</saay>\n" +
+            "    <sdclrq>2019-07-04</sdclrq>\n" +
+            "    <sar>admin</sar>\n" +
+            "    <sarq>2019-07-04</sarq>\n" +
+            "    <ajlyn>09_05085-1</ajlyn>\n" +
+            "    <sqsx>09_ZX0002-1</sqsx>\n" +
+            "    <ssrlx/>\n" +
+            "    <ajsj/>\n" +
+            "    <yzxah>(2019)鲁1302执5295号</yzxah>\n" +
+            "    <ayms>其他案由</ayms>\n" +
+            "    <scyj>09_ZX0028-1</scyj>\n" +
+            "    <scrq>2019-07-04</scrq>\n" +
+            "    <scr>371301zxqxy</scr>\n" +
+            "    <spr>371301zxqxy</spr>\n" +
+            "    <spyj>09_ZX0028-1</spyj>\n" +
+            "    <lasprq>2019-07-04</lasprq>\n" +
+            "    <ys_cbbm/>\n" +
+            "    <ys_jbfy>371301</ys_jbfy>\n" +
+            "    <ys_jbfymc>临沂市兰山区人民法院</ys_jbfymc>\n" +
+            "    <ys_yabs>198420191002005322</ys_yabs>\n" +
+            "    <ys_ycbbmbs/>\n" +
+            "    <ys_ycbbmmc>执行指挥中心</ys_ycbbmmc>\n" +
+            "    <ys_ycbrbs/>\n" +
+            "    <ys_ajlb>8</ys_ajlb>\n" +
+            "    <ys_ah>(2019)鲁1302执5295号</ys_ah>\n" +
+            "    <ys_larq>2019-06-17</ys_larq>\n" +
+            "    <ys_ay>3368</ys_ay>\n" +
+            "    <ys_aymc>其他案由</ys_aymc>\n" +
+            "    <ys_jarq/>\n" +
+            "    <ys_jafs/>\n" +
+            "    <ys_xh>1</ys_xh>\n" +
+            "    <DSR_List>\n" +
+            "      <A0>\n" +
+            "        <DSR_lx>09_01001-1</DSR_lx>\n" +
+            "        <DSR_ssdw_text>异议人(申请人)</DSR_ssdw_text>\n" +
+            "        <DSR_dsrMcchanged/>\n" +
+            "        <DSR_bgrpxh>1</DSR_bgrpxh>\n" +
+            "        <DSR_mc>主峰</DSR_mc>\n" +
+            "        <DSR_ssdw>09_ZX0032-1</DSR_ssdw>\n" +
+            "        <DSR_zjzl>09_00015-1</DSR_zjzl>\n" +
+            "        <DSR_lxdh>18733520021</DSR_lxdh>\n" +
+            "        <DSR_zjhm>372801196911041034</DSR_zjhm>\n" +
+            "        <DSR_mz/>\n" +
+            "        <DSR_xb>09_00003-1</DSR_xb>\n" +
+            "        <DSR_tssf>09_00012-255</DSR_tssf>\n" +
+            "        <DSR_tssfmc>无</DSR_tssfmc>\n" +
+            "        <DSR_dz>临沂市兰山区沂蒙路344号3号楼1单元301室</DSR_dz>\n" +
+            "      </A0>\n" +
+            "      <A1>\n" +
+            "        <DSR_lx>09_01001-1</DSR_lx>\n" +
+            "        <DSR_ssdw_text>异议人(申请人)</DSR_ssdw_text>\n" +
+            "        <DSR_dsrMcchanged/>\n" +
+            "        <DSR_bgrpxh>2</DSR_bgrpxh>\n" +
+            "        <DSR_mc>临沂正宇商贸有限公司</DSR_mc>\n" +
+            "        <DSR_ssdw>09_ZX0032-1</DSR_ssdw>\n" +
+            "        <DSR_zjzl>09_00015-1</DSR_zjzl>\n" +
+            "        <DSR_lxdh>18733520022</DSR_lxdh>\n" +
+            "        <DSR_zjhm/>\n" +
+            "        <DSR_mz/>\n" +
+            "        <DSR_xb/>\n" +
+            "        <DSR_tssf>09_00012-255</DSR_tssf>\n" +
+            "        <DSR_tssfmc>无</DSR_tssfmc>\n" +
+            "        <DSR_dz>临沂市兰山区柳青街道办事处颐高上海街1号楼B座601室</DSR_dz>\n" +
+            "      </A1>\n" +
+            "    </DSR_List>\n" +
+            "  </case>\n" +
+            "</GtRecord>\n" +
+            "\n";
+}
---
+++ src/main/java/cn/com/util/Word2HtmlUtils.java
@@ -0,0 +1,176 @@
+package cn.com.util;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.converter.PicturesManager;
+import org.apache.poi.hwpf.converter.WordToHtmlConverter;
+import org.apache.poi.hwpf.usermodel.PictureType;
+import org.apache.poi.xwpf.converter.core.FileImageExtractor;
+import org.apache.poi.xwpf.converter.core.FileURIResolver;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLConverter;
+import org.apache.poi.xwpf.converter.xhtml.XHTMLOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+
+public class Word2HtmlUtils {
+
+    public static void main(String[] args) {
+//        String filepath = "C:\\Users\\zhum\\Desktop\\";
+//        String fileName = "NTM法官工作平台数据库设计v1.4.9.docx";
+//        String htmlName = "test.html";
+        //wordDocx2Html(filepath,fileName,htmlName);
+
+
+        String filepath = "C:\\Users\\zhum\\Desktop\\";
+        String imagepath = "C:\\Users\\zhum\\Desktop\\image";
+        String fileName = "笔录模板.doc";
+        String htmlName = "testdoc.html";
+        wordDoc2Html(filepath,imagepath,fileName,htmlName);
+
+    }
+
+
+    /**
+     * @author zhum
+     * @date 2019年7月4日 10:48:00
+     * @description docx格式的word转换为html
+     * @param filepath Word文件所在 和 生成的html文件保存 的绝对路径 例：C:\Users\zhum\Desktop\
+     * @param fileName Word文件的名称 例：NTM法官工作平台数据库设计v1.4.9.docx
+     * @param htmlName HTML的名称 例：test.html
+     * @return
+     */
+    public static boolean wordDocx2Html(String filepath,String fileName,String htmlName){
+        boolean flag = true;
+        final String file = filepath + fileName;
+        File f = new File(file);
+        try {
+            if (!f.exists()) {
+                flag = false;
+                System.out.println("Sorry File does not Exists!");
+            } else {
+                if (f.getName().endsWith(".docx") || f.getName().endsWith(".DOCX")) {
+
+                    // 1) 加载word文档生成 XWPFDocument对象
+                    InputStream in = new FileInputStream(f);
+                    XWPFDocument document = new XWPFDocument(in);
+
+                    // 2) 解析 XHTML配置 (这里设置IURIResolver来设置图片存放的目录)
+                    File imageFolderFile = new File(filepath);
+                    XHTMLOptions options = XHTMLOptions.create().URIResolver(new FileURIResolver(imageFolderFile));
+                    options.setExtractor(new FileImageExtractor(imageFolderFile));
+                    options.setIgnoreStylesIfUnused(false);
+                    options.setFragment(true);
+
+                    // 3) 将 XWPFDocument转换成XHTML
+                    OutputStream out = new FileOutputStream(new File(filepath + htmlName));
+                    XHTMLConverter.getInstance().convert(document, out, options);
+
+                    //也可以使用字符数组流获取解析的内容
+//                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//                XHTMLConverter.getInstance().convert(document, baos, options);
+//                String content = baos.toString();
+//                System.out.println(content);
+//                 baos.close();
+                } else {
+                    flag = false;
+                    System.out.println("Enter only MS Office 2007+ files");
+                }
+            }
+        } catch (IOException e){
+            flag = false;
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    /**
+     * @author zhum
+     * @date 2019年7月4日 11:17:17
+     * @description doc格式的word转换为html
+     * @param filepath Word文件所在 和 生成的html文件保存 的绝对路径 例：C:\Users\zhum\Desktop\
+     * @param imagepath 保存Word文档中的图片 例：C:\\Users\\zhum\\Desktop\\image
+     * @param fileName Word文件的名称 例：NTM法官工作平台数据库设计v1.4.9.docx
+     * @param htmlName HTML的名称 例：test.html
+     * @return
+     */
+    public static boolean wordDoc2Html(String filepath,final String imagepath,String fileName,String htmlName){
+        boolean flag = true;
+
+        final String file = filepath + fileName;
+
+        try {
+            InputStream input = new FileInputStream(new File(file));
+            HWPFDocument wordDocument = new HWPFDocument(input);
+            WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
+            //设置图片存放的位置
+            wordToHtmlConverter.setPicturesManager(new PicturesManager() {
+                public String savePicture(byte[] content, PictureType pictureType, String suggestedName, float widthInches, float heightInches) {
+                    File imgPath = new File(imagepath);
+                    if(!imgPath.exists()){//图片目录不存在则创建
+                        imgPath.mkdirs();
+                    }
+                    File file = new File(imagepath + suggestedName);
+                    try {
+                        OutputStream os = new FileOutputStream(file);
+                        os.write(content);
+                        os.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return imagepath + suggestedName;
+                }
+            });
+
+            //解析word文档
+            wordToHtmlConverter.processDocument(wordDocument);
+            Document htmlDocument = wordToHtmlConverter.getDocument();
+
+            File htmlFile = new File(filepath + htmlName);
+            OutputStream outStream = new FileOutputStream(htmlFile);
+
+            //也可以使用字符数组流获取解析的内容
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        OutputStream outStream = new BufferedOutputStream(baos);
+
+            DOMSource domSource = new DOMSource(htmlDocument);
+            StreamResult streamResult = new StreamResult(outStream);
+
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer serializer = factory.newTransformer();
+            serializer.setOutputProperty(OutputKeys.ENCODING, "utf-8");
+            serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+            serializer.setOutputProperty(OutputKeys.METHOD, "html");
+
+            serializer.transform(domSource, streamResult);
+
+            //也可以使用字符数组流获取解析的内容
+//        String content = baos.toString();
+//        System.out.println(content);
+//        baos.close();
+            outStream.close();
+        } catch (IOException e){
+            flag = false;
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            flag = false;
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            flag = false;
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            flag = false;
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+
+}
